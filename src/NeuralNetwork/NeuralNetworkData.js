c0ca0ef344d38fe4107c5c28b77cda9888cd4d7d
colormotor <drand48@gmail.com>
2022-12-13 14:50:42 +0000

    test normalization hack

--- src/NeuralNetwork/NeuralNetworkData.js
+++ src/NeuralNetwork/NeuralNetworkData.js
@@ -135,10 +135,16 @@ class NeuralNetworkData {
         const dataAsArray = dataRaw.map(item => item[xsOrYs][k]);
         inputMeta[k].min = nnUtils.getMin(dataAsArray);
         inputMeta[k].max = nnUtils.getMax(dataAsArray);
+        if (inputMeta[k].max - inputMeta[k].min < Number.EPSILON*10){
+          inputMeta[k].max = inputMeta[k] + 1;
+        }
       } else if (inputMeta[k].dtype === 'array') {
         const dataAsArray = dataRaw.map(item => item[xsOrYs][k]).flat();
         inputMeta[k].min = nnUtils.getMin(dataAsArray);
         inputMeta[k].max = nnUtils.getMax(dataAsArray);
+        if (inputMeta[k].max - inputMeta[k].min < Number.EPSILON*10){
+          inputMeta[k].max = inputMeta[k] + 1;
+        }
       }
     });
 
